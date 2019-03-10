da4d9d57bd23da7acfc967d1b7fc6a0899353f48
Arthur199212 <46008463+Arthur199212@users.noreply.github.com>
2019-03-10 02:20:30 +0200

    fix: fix the bug and now it work

--- src/index.js
+++ src/index.js
@@ -1,84 +1,122 @@
-module.exports = function check(str, bracketsConfig) {
-  let stack = [];
-  let examStr = [];
-  let last = [];
-  let closeBrct = [];
-  
-  // Examination string  
+function check(str, bracketsConfig) {
+  let pattern = [];
+  let stackUniq = [];
+  let stackEqual = [];
+
   for (let i = 0; i < bracketsConfig.length; i++) {
       for (let j = 0; j < bracketsConfig[i].length; j++) {
-        examStr.push(bracketsConfig[i][j]);
+          pattern.push(bracketsConfig[i][j]);
       }
   }
 
-  // All characters can be used
+  const patternStr = pattern = pattern.join('');
+  
+  // Start
   for (let i = 0; i < str.length; i++) {
-      if ( examStr.indexOf(str[i]) > -1 ) {
-      } else {return false};
-  }
 
-  // Solve task
-  for (let i = 0; i < str.length; i++) {
-    if (isItOpenningBracket( str[i] )) {
+      if ( isOpen(i)) {
+          if ( isEqual(i) ) { // Opening
+              stackEqual.push(str[i]); // Equal
+          } else {
+              stackUniq.push(str[i]); // Uniq
+          }
+      } else { // Closing
+          if (patternStr.indexOf(str[i]) < 0 || stackUniq == 0) {
+              return false;
+          }
 
-        if ( stack.indexOf( str[i] ) > -1 && !(/([\[\]\(\)\{\}])/.test( str[i] )) ){
-            
-            console.log( 'hello' );
-
-            last = stack.pop();       
-            closeBrct = str[i];  
-                   
-            if ( !compare(last, closeBrct) ) {
-                return false;
-            } else {
-                return true;
-            }
-
-        } else {
-        stack.push( str[i] );
-        }
-
-    } else {
-        // If stack is empty = return false
-        if ( stack.length == 0 ) {
-           return false;
-        }
-
-        last = stack.pop();       
-        closeBrct = str[i];  
-               
-        if ( !compare(last, closeBrct) ) {
-            return false;
-        }
-         
+          let n = patternStr.indexOf(str[i]);
+          
+          if ( patternStr[n-1] != stackUniq.pop() ) {
+              return false;        
+          }
       }
-    }
-
-  // Compare open and close brakets
-  function compare(open, close) {
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        if ( bracketsConfig[i][0] == last && bracketsConfig[i][1] == closeBrct ) {
-            return true;
-        }
-    }
-
-    return false;
   }
 
-  // Is it opening bracket
-  function isItOpenningBracket(symbol) {
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        if ( symbol == bracketsConfig[i][0] ) {
-            return true;
-        }
-    }
-    return false;
+  // Final
+  if ( stackUniq.length != 0 ) {
+      return false;
   }
 
-  // If stack is not empty - return false
-  if ( !stack.length == 0 ) {
-    return false;
+  if ( stackEqual.length % 2 != 0 ) {
+      return false;
+  }
+
+  if (!finalCheck()) {
+      return false;
   }
 
   return true;
+
+
+  // Opening Br?
+  function isOpen(i) {
+      if (patternStr.indexOf(str[i]) % 2 == 0) {
+          return true;
+      } 
+      return false;
+  }
+
+  // Opening Br == Closing Br ?
+  function isEqual(i) {
+      let n = patternStr.indexOf(str[i]);
+
+      if ( str[i] == patternStr[patternStr.indexOf(str[i])+1] ) {
+          return true;
+      }
+      return false;
+  }
+
+  function finalCheck() {
+      let k = 0;
+      let a = [];
+      let b = [];
+      let lastCheck = [];
+
+      for (let i = 0; i < str.length; i = i + 2) {
+          if (patternStr[i] == patternStr[i+1]) {
+              lastCheck.push(patternStr[i]);
+          }
+      }
+
+
+
+      for (let i = 0; i < str.length; i++) {
+          k = str.indexOf(lastCheck[0], i);
+          if (k < 0) {
+              break;
+          }
+          a.push(k);
+          i = k;
+      }
+
+      if (lastCheck.length > 1) {
+          for (let i = 0; i < str.length; i++) {
+              k = str.indexOf(lastCheck[1], i);
+              if (k < 0) {
+                  break;
+              }
+              b.push(k);
+              i = k;
+          }
+
+          b.reverse();
+
+          for (let i = 0; i < b.length-1; i = i + 2) {
+              if ((b[i] - b[i+1]) % 2 == 0) {
+                  return false;
+              }
+          }
+      }
+
+      a.reverse();
+
+      for (let i = 0; i < a.length-1; i = i + 2) {
+          if ((a[i] - a[i+1]) % 2 == 0) {
+              return false;
+          }
+      }
+      
+      return true;
+  }
 }
